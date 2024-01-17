1cadddad327a94fad726bfc597bb54c9dc7912c4
agoodbook <beher0@foxmail.com>
2024-01-18 01:10:21 +0800

    init

---
+++ src/components/Button/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+var Button = function (_a) {
+    var label = _a.label, onClick = _a.onClick;
+    return (React.createElement("button", { onClick: onClick }, label));
+};
+export default Button;
